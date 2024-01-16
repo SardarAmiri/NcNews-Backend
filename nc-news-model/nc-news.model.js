05587327aa63be35aee6fc1778b2312a4196af2c
SardarAmiri <sardaramiri1122@gmail.com>
2024-01-16 13:57:49 +0000

    updated task 3

--- nc-news-model/nc-news.model.js
+++ nc-news-model/nc-news.model.js
@@ -4,6 +4,7 @@ module.exports.fetchAllTopics = () => {
     // console.log('I am in model')
     return db.query("SELECT * FROM topics")
     .then((topicsData) => {
+        console.log(topicsData)
         return topicsData.rows
     })
 }
