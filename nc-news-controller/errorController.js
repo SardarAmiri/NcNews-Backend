8713aed27fe47e45d3660e7289e16f957d7c22df
SardarAmiri <sardaramiri1122@gmail.com>
2024-05-19 01:52:43 +0100

    updated file

--- nc-news-controller/errorController.js
+++ nc-news-controller/errorController.js
@@ -1,19 +1,21 @@
 const CustomError = require("../utils/customError");
 
 module.exports = (error, req, res, next) => {
-    if(error.code === '22P02' || error.code === '23502' || error.code === '42703'){
-        const err = new CustomError('Bad request', 400)
-        err.statusCode = err.statusCode || 500;
-        res.status(err.statusCode).send({msg: err.message})
-    }
-    else if(error.code === '23503'){
-        const err = new CustomError(`No user found for article_id`, 404)
-        err.statusCode = err.statusCode || 500;
-        res.status(err.statusCode).send({msg: err.message})
-    }
-    else{
-        error.statusCode = error.statusCode || 500;
-        res.status(error.statusCode).send({msg: error.message})
-    }
-    
-}
+  console.log(error);
+  if (
+    error.code === "22P02" ||
+    error.code === "23502" ||
+    error.code === "42703"
+  ) {
+    const err = new CustomError("Bad request", 400);
+    err.statusCode = err.statusCode || 500;
+    res.status(err.statusCode).send({ msg: err.message });
+  } else if (error.code === "23503") {
+    const err = new CustomError(`No user found for article_id`, 404);
+    err.statusCode = err.statusCode || 500;
+    res.status(err.statusCode).send({ msg: err.message });
+  } else {
+    error.statusCode = error.statusCode || 500;
+    res.status(error.statusCode).send({ msg: error.message });
+  }
+};
